460bd2827e62fdc59e12f9a36c374e0e0ed68756
Jason Lee <jalee@cs.fsu.edu>
2016-12-04 17:12:20 -0500

    Multiple fixes
    Abstracted config line parser
    Fixed MPI DataTypes

--- traffic/__init__.py
+++ traffic/__init__.py
@@ -22,3 +22,14 @@ patterns = {
     'rand'        : rand.rand,
     'random'      : rand.rand,
 }
+
+# any extra pattern specific data to extract
+# if a pattern doesn't have data, it can return
+# an empty array, or not even be in this dict
+pattern_specific = {
+    'perm'        : lambda line : {'skip'  : int(line[7]),},
+    'permutation' : lambda line : {'skip'  : int(line[7]),},
+    'shift'       : lambda line : {'shift' : int(line[7]),},
+    'rand'        : lambda line : {'skip'  : int(line[7]),},
+    'random'      : lambda line : {'skip'  : int(line[7]),},
+}
